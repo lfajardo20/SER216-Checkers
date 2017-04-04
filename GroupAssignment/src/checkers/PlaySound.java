61064c01e82fc678f7bee480cb0c0428ae07ff53
lfajardo20 <lfajardo@asu.edu>
2017-04-04 08:05:32 -0700

    Add files via upload

---
+++ GroupAssignment/src/checkers/PlaySound.java
@@ -0,0 +1,90 @@
+package checkers;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.*;
+
+public class PlaySound extends Thread {
+
+	private String filename;
+
+	private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
+
+	enum Position {LEFT, RIGHT, NORMAL}
+
+	public PlaySound(String wavfile) {
+		filename = wavfile;
+	}
+
+	public void run() {
+		if(Checkers.silent) return;
+		File soundFile = new File(filename); 
+		System.out.println("File is being implemented.");// TEST
+		
+		if(filename.isEmpty()==true){
+			System.out.println("wave file not found: " + filename);
+		}
+
+
+		if (soundFile.exists()) {
+			System.err.println("Wave file not found test: " + filename); // exception is right
+			return;
+		}
+
+
+		AudioInputStream audioInputStream;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+		} catch (UnsupportedAudioFileException e1) {
+			e1.printStackTrace();
+			return;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		AudioFormat format = audioInputStream.getFormat();
+		SourceDataLine auline;
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+
+		try {
+			auline = (SourceDataLine) AudioSystem.getLine(info);
+			auline.open(format);
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+			return;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+
+		if (auline.isControlSupported(FloatControl.Type.PAN)) {
+			FloatControl pan = (FloatControl) auline.getControl(FloatControl.Type.PAN);
+		}
+
+		auline.start();
+		int nBytesRead = 0;
+		byte[] abData = new byte[EXTERNAL_BUFFER_SIZE];
+
+		try {
+			while (nBytesRead != -1) {
+				nBytesRead = audioInputStream.read(abData, 0, abData.length);
+				if (nBytesRead >= 0)
+					auline.write(abData, 0, nBytesRead);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			auline.drain();
+			auline.close();
+		}
+
+	}
+}
